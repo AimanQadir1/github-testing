716de809877105808c92049283cc037a7d817572
AimanQadir1 <aimanqadirr@gmail.com>
2024-11-04 15:24:28 +0100

    Initial Commit

---
+++ github testing/github_testingApp.swift
@@ -0,0 +1,17 @@
+//
+//  github_testingApp.swift
+//  github testing
+//
+//  Created by Aiman Qadir on 04/11/24.
+//
+
+import SwiftUI
+
+@main
+struct github_testingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
